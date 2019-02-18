02a2e19d24c67d8b3949b6482ceb2b463f999a73
Henrique Pereira Batista <h.p.batista20@gmail.com>
2019-02-18 13:15:37 +0000

    map displaying current city and working on dinamically render markers

--- src/components/mapComponents/mapComponent.js
+++ src/components/mapComponents/mapComponent.js
@@ -4,18 +4,54 @@ import MapMarker from './mapMarker.js'
 
 const MapComponent = withScriptjs(withGoogleMap((props) =>{
 
-//   const singleMarker = props.markers.map( marker => <MapMarker
-//                     key={marker.id}
-//                     marker={marker}
-//                     location={{lat: marker.lat, lng: marker.lon}}
-//                   />);
+ 
+      const singleMarker = props.potholes.map( pothole => <MapMarker
+                    key={pothole.id}
+                    pothole={pothole}
+                    address={{address: pothole.location}}
+                  />);
+        // for (let i in props.potholes) {
+        // const data = props.potholes[i].location;
+        // console.log(data);
+        // }
+
+        // let data = Object.keys(obj).forEach(key => {
+        // console.log(obj[key]);
+        // });
+   
+       
+        let data = props.potholes.forEach(function(obj) {
+            console.log(obj.location);
+             });
+
+            //  let newData = props.potholes.map((pothole, i) => {
+            //     let objLocation = pothole[i].location;
+            //  })
+             
+       
+    
+
+    let geocoder = new window.google.maps.Geocoder();
+    geocoder.geocode( { 'address': 'Barker st'}, function(results, status) {
+            if (status === 'OK') {
+                console.log(results);
+            } else {
+                console.log('Geocode was not successful for the following reason: ' + status);
+            }
+ });
+
+ 
+
+
+
+                  
                   
   return (
       <GoogleMap
         defaultZoom={14}
         center={ { lat:  55.86515, lng: -4.25763 } }
         >
-        {/* {singleMarker} */}
+        {singleMarker}
       </GoogleMap>
     );
   }
--- src/containers/mapContainer.js
+++ src/containers/mapContainer.js
@@ -7,7 +7,7 @@ class MapContainer extends React.Component {
 	render() {
 		return (
 			<MapComponent
-				// potholes={this.props.potholes}
+				potholes={this.props.potholes}
 				googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyBUukPjxf553UolDbw2vuevNzC2hlzSu7I&v=3.exp&libraries=geometry,drawing,places`}
 				loadingElement={<div style={{ height: `100%` }} />}
 				containerElement={<div style={{ height: `600px`, width: `600px` }} />}
